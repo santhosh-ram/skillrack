bfb431fe3846737b6329cf9f6edfe95623f8ecd7
santhosh-ram <165742023+santhosh-ram@users.noreply.github.com>
2024-11-23 11:51:38 +0530

    Update day5.cpp

--- day5.cpp
+++ day5.cpp
@@ -1,3 +1,4 @@
+//Daily challenge//
 #include<iostream>
 #include<vector>
 #include<numeric>
@@ -23,3 +24,44 @@ int main()
     }
   return 0;
 }
+//Daily Test//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int l[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> l[i];
+    }
+    int count=0,a,b;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(l[i]>l[j])
+            {
+                count++;
+                a=l[i];
+                b=l[j];
+            }
+        }
+    }
+    cout << count << endl;
+    cout << a << " " << b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
