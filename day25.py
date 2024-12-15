7647c77722038ee4c761c0bb651d07b93f8f5171
santhosh-ram <165742023+santhosh-ram@users.noreply.github.com>
2024-12-15 10:28:26 +0530

    Update day25.py

--- day25.py
+++ day25.py
@@ -4,3 +4,14 @@ for i in range(b):
   for j in range(i+1):
     print(chr(((a+j-1)%26)+65),end=" ")
   print()  
+
+n=int(input())
+l=[True]*(n+1)
+l[0]=l[1]=False
+for i in range(2,int(n**0.5)+1):
+    if l[i]:
+        for j in range(i*i,n+1,i):
+            l[j]=False
+for i in range(5,len(l)-5):
+    if l[i] and l[i+6]:
+        print(i,i+6)
